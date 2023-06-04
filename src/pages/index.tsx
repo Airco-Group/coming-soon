294509c0f143a364f2e2f51de9ceeea899fd4116
Cortlin Sawyer <cortlin.sawyer@gmail.com>
2023-06-03 23:53:19 -0700

    adding toast message on form submit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,24 @@
 import Head from "next/head";
 import Image from "next/image";
 import bgImage from "../../public/comingSoonBg.jpg";
+import { FormEvent, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function Home() {
+  const [showToast, setShowToast] = useState<boolean>(false);
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    setShowToast(true);
+  };
   return (
     <>
       <Head>
         <title>Airco, Inc.</title>
-        <meta name="description" content="Temporary Landing page for Airco, Inc." />
+        <meta
+          name="description"
+          content="Temporary Landing page for Airco, Inc."
+        />
         <link rel="icon" href="/favicon.png" />
       </Head>
       <main
@@ -21,7 +32,7 @@ export default function Home() {
             width="450"
             height="100"
           />
-          <div className="grid grid-cols-2 gap-8 sm:grid-cols-4 md:gap-12 items-center">
+          <div className="grid grid-cols-2 items-center gap-8 sm:grid-cols-4 md:gap-12">
             <Image
               src="/airco-equipment.png"
               alt="primary Logo"
@@ -63,9 +74,13 @@ export default function Home() {
           </div>
 
           <div>
-            <form name="Contact Us" action="/success" data-netlify="true">
+            <form
+              name="Contact Us"
+              onSubmit={(e) => handleSubmit(e)}
+              data-netlify="true"
+            >
               <div className="flex gap-4">
-              <input type="hidden" name="form-name" value="Contact Us" />
+                <input type="hidden" name="form-name" value="Contact Us" />
                 <label className="mb-2 block text-sm font-bold text-gray-700">
                   Name:
                   <input
@@ -108,7 +123,59 @@ export default function Home() {
             </form>
           </div>
         </div>
+        <AnimatePresence>
+          {showToast && (
+            <ToastMessage
+              message="Email successfully sent!"
+              setShowToast={setShowToast}
+            />
+          )}
+        </AnimatePresence>
       </main>
     </>
   );
 }
+
+type toastProps = {
+  message: string;
+  setShowToast: (b: boolean) => void;
+};
+
+const ToastMessage = ({ message, setShowToast }: toastProps) => {
+  return (
+    <motion.div
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      exit={{ opacity: 0 }}
+    >
+      <div className="absolute bottom-0 left-1/2 mb-10 -translate-x-1/2 transition delay-100 ease-in-out">
+        <div
+          className="max-w-xs rounded-md border bg-white shadow-lg dark:border-gray-700 dark:bg-gray-800"
+          role="alert"
+        >
+          <div className="flex gap-4 p-2 sm:p-4">
+            <h3 className="text-xs font-semibold text-gray-800 dark:text-white sm:text-base">
+              {message}
+            </h3>
+            <button onClick={() => setShowToast(false)}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="h-6 w-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+    </motion.div>
+  );
+};
